1f82042c9d736ae50670fbe3fb73db429c712e4e
Armando Toaquiza <luis.toaquiza5731@utc.edu.ec>
2026-07-28 13:04:32 -0500

    Roles modificado

--- Sistema/settings.py
+++ Sistema/settings.py
@@ -58,7 +58,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'luistoaquiza108@gmail.com')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'fczm edob hxwi hikm')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'dtxp fgzf tgjf hkjo')
 
 # --- Application definition ---
 INSTALLED_APPS = [
