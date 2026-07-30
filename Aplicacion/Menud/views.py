95822f0ef0e46c891f5e6cc18f026ac1ba7b9bd7
Armando Toaquiza <luis.toaquiza5731@utc.edu.ec>
2026-07-29 21:37:05 -0500

    opcion personalizar plato

--- Aplicacion/Menud/views.py
+++ Aplicacion/Menud/views.py
@@ -1416,7 +1416,7 @@ def personalizar_pedido(request):
     return render(request, 'personalizar_pedido.html', {'platos': platos})
 
 @cliente_required
-def confirmar-pedido-personalizado(request):
+def confirmar_pedido_personalizado(request):
     if request.method != 'POST':
         return JsonResponse({'success': False, 'message': 'Método no permitido'}, status=405)
     
